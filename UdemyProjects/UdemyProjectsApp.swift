be9a2b97112796e254e21f98bcb3894790a886ea
Kevin Massol <kevin.massol@icloud.com>
2024-01-29 08:17:21 +0100

    Initial Commit

---
+++ UdemyProjects/UdemyProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemyProjectsApp.swift
+//  UdemyProjects
+//
+//  Created by KEVIN on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UdemyProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
